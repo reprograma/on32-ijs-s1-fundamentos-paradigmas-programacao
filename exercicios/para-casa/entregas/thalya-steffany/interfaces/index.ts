39e67d07a078cc197f8428370df0ff7561e81f92
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-06-08 20:55:52 -0300

    feat(para-casa): cria interfaces do exercício 1

---
+++ exercicios/para-casa/entregas/thalya-steffany/interfaces/index.ts
@@ -0,0 +1,10 @@
+export interface IProdutoEcologico {
+  nome: string;
+  preco: number;
+}
+
+export interface IItemDecoracaoIDimensoes {
+  largura: number;
+  altura: number;
+  profundidade: number;
+}

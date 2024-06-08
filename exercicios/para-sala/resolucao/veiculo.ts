047700f4a019fd7d398f6e155370c0d8610b4913
mellyssamnds <mellyssah.mendes@live.com>
2024-06-08 15:54:45 -0300

    feat: add class exercises

---
+++ exercicios/para-sala/resolucao/veiculo.ts
@@ -0,0 +1,23 @@
+interface Veiculo {
+    marca:string;
+    modelo:string;
+    ano:number;
+    exibirInformacoes():string
+}
+
+class Automovel implements Veiculo{
+    marca:string;
+    modelo:string;
+    ano:number;
+
+    constructor(marca:string, modelo:string, ano:number){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    exibirInformacoes(): string {
+        return `Marca:${this.marca}, Modelo:${this.modelo}, Ano:${this.ano}`
+    }
+}
+const myCar = new Automovel("Hiunday", "Creta", 2024)
+console.log(myCar.exibirInformacoes())

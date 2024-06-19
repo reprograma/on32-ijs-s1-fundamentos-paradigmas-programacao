2938a133a1308d65b4a8daa84d202048c6a352dc
i-malur <marialuizafernandes383@gmail.com>
2024-06-19 20:05:23 -0300

    Atualizando código de alimentosOrganicos

--- exercicios/para-casa/entregas/desafio-semanal-MariaLuiza/index.ts
+++ exercicios/para-casa/entregas/desafio-semanal-MariaLuiza/index.ts
@@ -3,14 +3,14 @@ import { ProdutosLimpezaBiodegradavel } from "./ProdutosLimpeza";
 import { ItemDeDecoracaoSustentavel } from "./itensDecoracao";
 
 // Instanciando um alimento
-const maca = new AlimentosOrganicos("Morango Orgânico", 2.99, new Date("2024-06-24"), ["Morango"]);
-console.log(`\n Ficha Técnica -- ${maca.nome}\n ${maca.exibirAlimento()}`)
+const alimentoOrganico = new AlimentosOrganicos("Morango Orgânico", 2.99, new Date("2024-06-24"), ["Morango"]);
+console.log(`\n Ficha Técnica -- ${alimentoOrganico.nome}\n ${alimentoOrganico.exibirAlimento()}`)
 
 // Instanciando um produto de limpeza
-const lava_roupas = new ProdutosLimpezaBiodegradavel("Lava Roupas - 3 unidades", 73.22, 300);
-console.log(`\n Ficha Técnica -- ${lava_roupas.nome}\n ${lava_roupas.exibirProduto()}`)
+const produtoLimpeza = new ProdutosLimpezaBiodegradavel("Lava Roupas - 3 unidades", 73.22, 300);
+console.log(`\n Ficha Técnica -- ${produtoLimpeza.nome}\n ${produtoLimpeza.exibirProduto()}`)
 
 // Instanciando um item de decoração - com variável registrando as dimensões
-const dimensoesTigela = {largura: 12.5, altura: 3.5, profundidade:12.5};
-const tigela = new ItemDeDecoracaoSustentavel("Tigela 100 un", 150.00, "Bagaço Cana de Açúcar Biodegradável", dimensoesTigela);
-console.log(`\n Ficha Técnica -- ${tigela.nome}\n ${tigela.exibirItem()}`)
+const dimensoesProduto = {largura: 12.5, altura: 3.5, profundidade:12.5};
+const produtoDecoracao = new ItemDeDecoracaoSustentavel("Tigela 100 un", 150.00, "Bagaço Cana de Açúcar Biodegradável", dimensoesProduto);
+console.log(`\n Ficha Técnica -- ${produtoDecoracao.nome}\n ${produtoDecoracao.exibirItem()}`)
